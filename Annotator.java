72d3bd446e5be1b3bba576a2f01020d815225e62
sarahdae <sarah@daenel.info>
2016-06-14 11:20:40 +0000

    Add files via upload
    Within this class the annotators will start working with the given files.

---
+++ Annotator.java
@@ -0,0 +1,43 @@
+package networks;
+import edu.stanford.nlp.ie.util.RelationTriple;
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.naturalli.NaturalLogicAnnotations;
+import edu.stanford.nlp.util.CoreMap;
+import java.util.*;
+
+/**
+ * This class annotates a previously chosen and altered text file.
+ * (A output json/ xml (?) file is generated.)
+ * @author Sarah Dänel
+ */
+public class Annotator {
+   
+
+    public static void main(String[] args) throws Exception {
+        // Create the Stanford CoreNLP pipeline
+        Properties props = new Properties();
+        props.setProperty("annotators", "tokenize,ssplit,pos,lemma,ner,parse,dcoref,relation,depparse,natlog,openie,quote");
+        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+
+        // Annotate an example document.
+        Annotation doc = new Annotation("Obama was born in Hawaii. He is our president.");
+        pipeline.annotate(doc);
+
+        // Loop over sentences in the document
+        for (CoreMap sentence : doc.get(CoreAnnotations.SentencesAnnotation.class)) {
+            // Get the OpenIE triples for the sentence
+            Collection<RelationTriple> triples = sentence.get(NaturalLogicAnnotations.RelationTriplesAnnotation.class);
+            // Print the triples
+            for (RelationTriple triple : triples) {
+                System.out.println(triple.confidence + "\t"
+                        + triple.subjectLemmaGloss() + "\t"
+                        + triple.relationLemmaGloss() + "\t"
+                        + triple.objectLemmaGloss());
+            }
+            
+        }
+    }
+}
+
