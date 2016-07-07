01cd746db6548dac54b71743f5fa666f8985da3f
AnRueKe <anke.rueb@t-online.de>
2016-07-07 14:49:15 +0200

    CheckChars.java
    diese Klasse enthält eine Methode, die einen Filter (=nnp liste aus den Preprocessed files) und eine charakterliste nimmt, und die beiden abgleicht,
    und anschließend eine reduzierte Liste von nnps enthält (nur die, deren Lemmas in der Personenliste vorkommen)

---
+++ CheckChars.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+public class CheckChars {
+
+	/**
+	 * Klasse die die nnp Liste mit den von Hand erstellen Charakterlisten abgleicht, und alle nnps rauswirft, 
+	 * die nicht in
+	 * der Charakterliste vorkommen.
+	 */
+	ArrayList<String[]> characters = new ArrayList();
+	
+	public CheckChars(Filter nnpList, String charListFile) {
+			characters = this.check(nnpList, charListFile);
+		}
+	
+	private ArrayList check(Filter nnpList, String charListFile) {
+		try {
+			BufferedReader read = new BufferedReader(new FileReader(new File("charListFile")));
+			String line;
+			ArrayList nnps = nnpList.getNNPS();
+			
+			for(int i = 0; i < nnps.size(); i++)//go through list of filtered nnps
+			{
+				String[] lemma = (String[]) nnps.get(i);//lemma from filtered nnps
+				
+				while((line = read.readLine()) != null)//go through file of correct characters
+				{
+					String[] splitline = line.split(",");
+					
+					for(String nickname : splitline)
+					{
+						if(nickname.equals(lemma[2]))//is the lemma of this nnp one of the nicknames in that line? 
+						{
+						     if(!characters.contains(nnps.get(i)))
+						     {
+						    	 characters.add((String[])nnps.get(i));
+						     }
+
+						}
+					}
+				}
+			}
+		} catch (IOException e) {
+			System.out.println("File not found.");
+		}
+		
+		return characters;
+	}
+		
+	public ArrayList getCheckedCharacters() {
+		return characters;
+	}
+
+	public static void main(String[] args) {
+		
+	}
+	
+}
